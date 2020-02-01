c21f0fd4970e477c6688dfead75d405d3fd81daf
Micheal Kayode <kayodemicheal159@gmail.com>
2020-02-01 10:27:18 +0100

    connect everything together

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './header/Header';
+import SideMenu from './side/SideMenu';
+import Main from './main/Main';
+import Pagination from '../components/pagination/Pagination';
+
+const total = 20;
+const start = 1;
+const end = 10;
 
 function App() {
+  const [state, setState] = useState(1);
+  const noOfPage = 2;
+
+  const setSelected = (n, selected) => {
+    const s = Number(selected);
+
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(Number(n))) {
+      if (n === 'Previous' && s > 1) {
+        setState(s - 1);
+      } else if (n === 'Next' && s < noOfPage) {
+        setState(s + 1);
+      }
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`${n} clicked`);
+      setState(n);
+    }
+  };
+
   return (
-    <div className="App">
-      <header>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      <div className="second">
+        <SideMenu />
+        <Main />
+      </div>
+
+      <div className="footer">
+        <div className="propo" />
+
+        <div className="table-footer">
+          <p>{`Showing ${start} to ${end} of ${total} entries`}</p>
+
+          <Pagination
+            noOfPage={noOfPage}
+            selected={state.toString()}
+            onClick={setSelected}
+          />
+        </div>
+      </div>
     </div>
   );
 }
